0adca82d92eb986c130c14e18ffb0ac75c6e7cbc
Gabriel Fiandanese De Almeida Massa <gabriel.massa@grupomadero.com.br>
2026-05-20 17:39:38 -0300

    setup inicial da arquitetura

---
+++ Monitoramento.Core/Models/SystemStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monitoramento.Core.Models
+{
+    public class SystemStatus
+    {
+        public string SystemName { get; set; } = string.Empty;
+        public bool IsOnline { get; set; }
+        public int ResponseMs { get; set; }
+        public DateTime CheckedAt { get; set; }
+        public string? ErrorMessage { get; set; }
+    }
+}
